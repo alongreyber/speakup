e86c54416b307605fef55788751a8b45dd7d6c5c
Alon Greyber <alongreyber@gmail.com>
2017-10-10 00:19:17 -0400

    Moved to ad-hoc SSL but stil broken

--- web/main.py
+++ web/main.py
@@ -5,4 +5,5 @@ context.use_privatekey_file('app/self-signed-tmp.key')
 context.use_certificate_file('app/self-signed-tmp.crt')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True,
+            ssl_context='ad-hoc')
