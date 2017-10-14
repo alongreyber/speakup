9f1766b30754401d446e924041d437254e967620
Alon Greyber <alongreyber@gmail.com>
2017-10-14 01:19:02 -0400

    Switched SSL to use Ad-Hoc for dev. Production might not work.

--- web/app/static/mediaRecording.js
+++ web/app/static/mediaRecording.js
@@ -1,18 +1,16 @@
-navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
 var mediaConstraints = {
     audio: true,
     video: true
 };
+navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
 function onMediaError(e) {
     console.error('media error', e);
 }
 
-console.log("Hello from a script");
 function onMediaSuccess(stream) {
     var mediaRecorder = new MediaStreamRecorder(stream);
     mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
     mediaRecorder.ondataavailable = function (blob) {
-        // POST/PUT "Blob" using FormData/XHR2
         var blobURL = URL.createObjectURL(blob);
         document.write('<a href="' + blobURL + '">' + blobURL + '</a>');
     };
@@ -29,9 +27,8 @@ function upload(blob) {
     formData.append('video-filename', file.name);
     formData.append('video-blob', file);
 
-    makeXMLHttpRequest('https://path-to-your-server/save.php', formData, function() {
-        var downloadURL = 'https://path-to-your-server/uploads/' + file.name;
-        console.log('File uploaded to this path:', downloadURL);
+    makeXMLHttpRequest('https://submit_gentle', formData, function() {
+        console.log('File uploaded');
     });
 }
 function onMediaError(e) {
--- web/main.py
+++ web/main.py
@@ -1,9 +1,4 @@
 from app import app
-from OpenSSL import SSL
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file('app/self-signed-tmp.key')
-context.use_certificate_file('app/self-signed-tmp.crt')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True,
-            ssl_context='ad-hoc')
+    app.run(host='0.0.0.0', debug=True, threaded=True, ssl_context='adhoc')
