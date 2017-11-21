1eed31e0f48a839508749cbf9b13f205d10397f4
Alon Greyber <alongreyber@gmail.com>
2017-11-21 18:29:46 -0500

    Gentle dockerfile now doesn't depend on gitmodules

--- gentle/install.sh
+++ gentle/install.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-git submodule init
-git submodule update
-
+# Specific branch of kaldi that gentle likes
+(cd ext && git clone --recursive https://github.com/kaldi-asr/kaldi &&
+    cd kaldi && git checkout 61510ca0d8d9b38096701227ee064a166816fcbe)
 ./install_deps.sh
 (cd ext && ./install_kaldi.sh)
+
 ./install_models.sh
 cd ext && make depend && make
