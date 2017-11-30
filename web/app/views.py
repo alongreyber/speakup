13be753e0352c1437168fdf42841ff97b95bd12b
Alon Greyber <alongreyber@gmail.com>
2017-11-30 01:15:06 -0500

    Web dockerfile now builds much faster on code changes. Started expanding the layout of the submission page. Form submission doesn't work yet.

--- web/app/views.py
+++ web/app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 import app.forms as Forms
 
 # Temporary
@@ -7,7 +7,8 @@ import socket
 
 @app.route('/submit_gentle', methods=['GET', 'POST'])
 def submit_gentle():
-    alignment_form = Forms.Alignment()
+    app.logger.info('Hit submit_gentle endpoint with method ' + request.method)
+    alignment_form = Forms.Alignment(request.form)
     if alignment_form.validate_on_submit():
         return render_template('gentle_result.html')
     return render_template('submit_gentle.html',
